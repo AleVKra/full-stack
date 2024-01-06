60528affb67cd3bf9af538ba6be44f494121effc
user <kavkras83@mail.ru>
2024-01-06 19:57:26 +0300

    -create React Functional Component - EmployeeComponent; -Add "Add Employee button in ListEmployeeComponent -Configure Route for EmployeeConponent -test the above changes

--- ems-frontend/src/App.jsx
+++ ems-frontend/src/App.jsx
@@ -4,9 +4,9 @@ import React from "react"
 import FooterComponent from "./components/FooterComponent"
 import HeaderComponent from "./components/HeaderComponent"
 import {BrowserRouter, Routes, Route} from "react-router-dom"
+import EmployeeComponent from "./components/EmployeeComponent";
 
 function App() {
-
   return (
     <>
       <BrowserRouter>
@@ -16,11 +16,12 @@ function App() {
           <Route path='/' element={<ListEmployeeComponent/>}> </Route>
           {/*// http://localhost:3000/employees*/}
           <Route path='/employees' element={<ListEmployeeComponent/>}> </Route>
+          {/*// http://localhost:3000//add-employee*/}
+          <Route path='/add-employee' element ={<EmployeeComponent/>}> </Route>
         </Routes>
         <FooterComponent/>
       </BrowserRouter>
     </>
   )
 }
-
 export default App
--- ems-frontend/src/components/ListEmployeeComponent.jsx
+++ ems-frontend/src/components/ListEmployeeComponent.jsx
@@ -1,20 +1,27 @@
 import React, {useEffect, useState} from 'react'
 import {listEmployees} from "../services/EmployeeService";
+import {useNavigate} from 'react-router-dom'
 
 const ListEmployeeComponent = () => {
 
   const [employees, setEmployees] = useState([])
+  const navigator = useNavigate();
 
   useEffect(() => {
-      listEmployees().then((response) => {
-        setEmployees(response.data);
-      }).catch(error => {
-        console.error(error);
-      })
-    }, [])
+    listEmployees().then((response) => {
+      setEmployees(response.data);
+    }).catch(error => {
+      console.error(error);
+    })
+  }, [])
+
+  function addNewEmployee() {
+    navigator('/add-employee')
+  }
 
   return (
     <div className='container'>
+      <button className='btn btn-primary mb-2' onClick={addNewEmployee}>Add Employee</button>
       <h2 className='text-center'> List of Employee</h2>
       <table className='table table-striped table-bordered'>
         <thead>
