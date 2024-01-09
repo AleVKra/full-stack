7bfd0b1fc1fccee85ace1b989939a15877afc3af
user <kavkras83@mail.ru>
2024-01-09 22:20:19 +0300

    -Use the useState hook to initialize state variables that will hold validation errors -Write a validation function that checks the form data and returns validation errors - Validation Form on Submission -Display Validation Errors -Add update button to list employees page - add route for update employee in app component - change page title dynamially (EmpliyeeComponent supports both Add and update)

--- ems-frontend/src/components/ListEmployeeComponent.jsx
+++ ems-frontend/src/components/ListEmployeeComponent.jsx
@@ -19,6 +19,9 @@ const ListEmployeeComponent = () => {
     navigator('/add-employee')
   }
 
+  function updateEmployee(id) {
+    navigator(`/edit-employee/${id}`)
+  }
   return (
     <div className='container'>
       <button className='btn btn-primary mb-2' onClick={addNewEmployee}>Add Employee</button>
@@ -30,6 +33,7 @@ const ListEmployeeComponent = () => {
           <th>Employee First Name</th>
           <th>Employee Last Name</th>
           <th>Employee Email Id</th>
+          <th>Actions</th>
         </tr>
         </thead>
         <tbody>
@@ -40,6 +44,10 @@ const ListEmployeeComponent = () => {
               <td>{employee.firstName}</td>
               <td>{employee.lastName}</td>
               <td>{employee.email}</td>
+              <td>
+                <button className='btn btn-info'
+                onClick={()=>updateEmployee(employee.id)}>Update</button>
+              </td>
             </tr>)
         }
         </tbody>
