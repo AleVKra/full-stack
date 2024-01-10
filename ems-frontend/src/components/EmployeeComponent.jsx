87759b22d7953f84412ff8d8df9aee79c0185cf0
user <kavkras83@mail.ru>
2024-01-10 22:19:50 +0300

    -In EmployeeService, write a code to call Get Employee REST API using axios. - Use useEffect hook to populate the employee data in the form for update. - In EmployeeService,write a code to call Update Employee REST API using axios - Change EmployeeComponent.saveOrUpdateEmployee() mathod to perform add and update employee operations

--- ems-frontend/src/components/EmployeeComponent.jsx
+++ ems-frontend/src/components/EmployeeComponent.jsx
@@ -1,5 +1,5 @@
-import React, {useState} from 'react'
-import {createEmployee} from '../services/EmployeeService'
+import React, {useState, useEffect} from 'react'
+import {createEmployee, getEmployee, updateEmployee} from '../services/EmployeeService'
 import {useNavigate, useParams} from 'react-router-dom';
 
 
@@ -9,7 +9,7 @@ const EmployeeComponent = () => {
   const [lastName, setLastName] = useState('');
   const [email, setEmail] = useState('');
 
-  const {id}= useParams();
+  const {id} = useParams();
   const [errors, setErrors] = useState({
     firstName: '',
     lastName: '',
@@ -19,17 +19,42 @@ const EmployeeComponent = () => {
 
   const navigator = useNavigate();
 
-  function saveEmployee(e) {
+  useEffect(() => {
+
+    if (id) {
+      getEmployee(id).then((response) => {
+        setFirstName(response.data.firstName);
+        setLastName(response.data.lastName);
+        setEmail(response.data.email);
+      }).catch(error => {
+        console.error(error);
+      })
+    }
+  }, [id])
+
+
+  function saveOrUpdateEmployee(e) {
     e.preventDefault();
 
     if (validateForm()) {
       const employee = {firstName, lastName, email}
       console.log(employee)
 
-      createEmployee(employee).then((response) => {
-        console.log(response.data);
-        navigator('/employees')
-      })
+      if (id) {
+        updateEmployee(id, employee).then((response) => {
+          console.log(response.data);
+          navigator('/employees');
+        }).catch(error => {
+          console.error(error);
+        })
+      } else {
+        createEmployee(employee).then((response) => {
+          console.log(response.data);
+          navigator('/employees')
+        }).catch(error => {
+          console.error(error);
+        })
+      }
     }
   }
 
@@ -60,11 +85,11 @@ const EmployeeComponent = () => {
     setErrors(errorsCopy);
     return valid;
   }
-  
+
   function pageTitle() {
-    if (id){
+    if (id) {
       return <h2 className='text-center'>Update Employee</h2>
-    }else {
+    } else {
       return <h2 className='text-center'>Add Employee</h2>
     }
   }
@@ -120,7 +145,7 @@ const EmployeeComponent = () => {
                 </input>
                 {errors.email && <div className='invalid-feedback'>{errors.email}</div>}
               </div>
-              <button className='btn btn-success' onClick={saveEmployee}> Submit</button>
+              <button className='btn btn-success' onClick={saveOrUpdateEmployee}> Submit</button>
             </form>
 
           </div>
